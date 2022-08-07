012fa454c78a2cd1d9411a1c16978862845b55b7
Eric Echemane <eechemane29@gmail.com>
2022-08-07 16:45:32 +0800

    User entity created

--- src/__clean__/@entities/user.entity.ts
+++ src/__clean__/@entities/user.entity.ts
@@ -1,8 +1,22 @@
-import mongoose from "mongoose";
-import Hasher from "utils/hasher";
-import { isValidEmail } from "utils/validators";
+import { Schema, model, SchemaTypes } from 'mongoose';
+import Hasher from 'utils/hasher';
+import { isValidEmail } from 'utils/validators';
 
-const userSchema = new mongoose.Schema({
+export interface IUser {
+    email: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    phone_number: string;
+    address: string;
+    wallet: number;
+    cart: [],
+    orders: [],
+    reviews: [],
+    created_at: Date;
+}
+
+const userSchema = new Schema<IUser>({
     email: {
         type: String,
         required: [true, 'Email is required'],
@@ -14,8 +28,59 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
-        required: [true, 'Password is required'],
-        minLength: [8, 'Password must be at least 8 characters']
+        required: [true, 'Password is required']
+    },
+    firstName: {
+        type: String,
+        required: [true, 'Firstname is required'],
+        minLength: [2, 'Firstname must be at least 2 characters']
+    },
+    lastName: {
+        type: String,
+        required: [true, 'Lastname is required'],
+        minLength: [2, 'Lastname must be at least 2 characters']
+    },
+    phone_number: {
+        type: String,
+        required: [true, 'Mobile is required'],
+        validate: {
+            validator: function (phoneNumber: string) {
+                return /^(09|\+639)\d{9}$/.test(phoneNumber);
+            }
+        }
+    },
+    address: {
+        type: String,
+        required: [true, 'Address is required'],
+        minLength: [20, 'Address must be a complete address']
+    },
+    wallet: {
+        type: Number,
+        default: 0,
+    },
+    cart: {
+        type: [{
+            product: {
+                type: SchemaTypes.ObjectId,
+                ref: 'Product'
+            },
+            price: {
+                type: Number,
+                greaterThan: [0, 'Price cannot be 0']
+            },
+            quantity: {
+                type: Number,
+                greaterThan: [0, 'Quantity cannot be 0']
+            }
+        }]
+    },
+    orders: {
+        type: [SchemaTypes.ObjectId],
+        ref: 'Order'
+    },
+    created_at: {
+        type: Date,
+        default: Date.now
     }
 });
 
@@ -24,6 +89,4 @@ userSchema.pre('save', function (next) {
     next();
 });
 
-const User = mongoose.model("User", userSchema);
-
-export default User;
+export default model('User', userSchema);
