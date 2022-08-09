7e439099b1f2e6a66b43227fcd50ca486845dc06
Eric Echemane <eechemane29@gmail.com>
2022-08-09 08:15:50 +0800

    validators passing

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,11 +1,41 @@
 import 'mocha';
 import { expect } from 'chai';
+import { isValidEmail, lengthOf, isAllNumeric } from './validators';
 
-import User from '@entities/user.entity';
+describe('validators', () => {
+    context('isValidEmail', () => {
+        it('should return true for valid emails', () => {
+            expect(isValidEmail('name@example.com')).to.be.true;
+        });
+        it('should return false for invalid emails', () => {
+            expect(isValidEmail('nameexample.com')).to.be.false;
+        });
+    });
 
-describe('User', () => {
-    it('should create a new user', () => {
-        const user = new User();
-        expect(user).to.be.an.instanceOf(User);
+    context('lengthOf', () => {
+        it('should return true for a string with length 4', () => {
+            expect(lengthOf('eric').is(4)).to.be.true;
+        });
+        it('should return false for a string with length not equal to 4', () => {
+            expect(lengthOf('kenneth').isNot(4)).to.be.true;
+        });
+        it('should return true for a string with length less than 10', () => {
+            expect(lengthOf('eric').isLessThan(10)).to.be.true;
+        });
+        it('should return true for a string with length greater than 5', () => {
+            expect(lengthOf('echemane').isGreaterThan(5)).to.be.true;
+        });
+        it('should return true for a string with spaces and with length of 5', () => {
+            expect(lengthOf('eri c', false).is(5)).to.be.true;
+        });
+    });
+
+    context('isAllNumeric', () => {
+        it('should return true for a string with only numbers', () => {
+            expect(isAllNumeric('12345')).to.be.true;
+        }).timeout(1000);
+        it('should return false for a string with letters', () => {
+            expect(isAllNumeric('abcde')).to.be.false;
+        }).timeout(1000);
     });
 });
